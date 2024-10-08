43907d8dc1a921eab636106552ab73d9a054e95c
Graham Watts <grahamcharleswatts@gmail.com>
2024-10-08 13:34:49 +0300

    AddSpectreConsole overload using IConfiguration
    Adds an overload for `LoggerFactoryExtensions.AddSpectreConsole` that accepts an `IConfiguration` so that the `SpectreConsoleLoggerOptions` can be bound against configuration.

--- src/Jaahas.Spectre.Extensions/Logging/LoggerFactoryExtensions.cs
+++ src/Jaahas.Spectre.Extensions/Logging/LoggerFactoryExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
@@ -87,6 +88,45 @@ namespace Jaahas.Spectre.Extensions.Logging {
 
             builder.AddSpectreConsole();
             builder.Services.Configure(configure);
+
+            return builder;
+        }
+
+
+        /// <summary>
+        /// Adds a logger to the builder that writes to the console using Spectre.Console.
+        /// </summary>
+        /// <param name="builder">
+        ///   The <see cref="ILoggingBuilder"/>.
+        /// </param>
+        /// <param name="configuration">
+        ///   The <see cref="IConfiguration"/> to bind the <see cref="SpectreConsoleLoggerOptions"/> 
+        ///   against.
+        /// </param>
+        /// <param name="configurationSection">
+        ///   The name of the configuration section to bind the options against.
+        /// </param>
+        /// <returns>
+        ///   The <see cref="ILoggingBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="builder"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="configuration"/> is <see langword="null"/>.
+        /// </exception>
+        public static ILoggingBuilder AddSpectreConsole(this ILoggingBuilder builder, IConfiguration configuration, string? configurationSection = "SpectreConsoleLogger") {
+            if (builder == null) {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            if (configuration == null) {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            builder.AddSpectreConsole();
+            builder.Services.Configure<SpectreConsoleLoggerOptions>(string.IsNullOrWhiteSpace(configurationSection)
+                ? configuration
+                : configuration.GetSection(configurationSection!));
 
             return builder;
         }
