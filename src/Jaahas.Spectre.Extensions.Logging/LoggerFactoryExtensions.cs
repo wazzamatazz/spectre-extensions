3d2420f281e2bd33463e4eb6f7432477863afb50
Graham Watts <grahamcharleswatts@gmail.com>
2024-11-20 13:54:02 +0200

    Add logging integration to new project

---
+++ src/Jaahas.Spectre.Extensions.Logging/LoggerFactoryExtensions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Jaahas.Spectre.Extensions.Logging {
+
+    /// <summary>
+    /// Extension methods for configuring logging using Spectre.Console.
+    /// </summary>
+    public static class LoggerFactoryExtensions {
+
+        /// <summary>
+        /// Adds a logger to the factory that writes to the console using Spectre.Console.
+        /// </summary>
+        /// <param name="factory">
+        ///   The <see cref="ILoggerFactory"/>.
+        /// </param>
+        /// <param name="options">
+        ///   The options to use when creating the logger.
+        /// </param>
+        /// <returns>
+        ///   The <see cref="ILoggerFactory"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="factory"/> is <see langword="null"/>.
+        /// </exception>
+        public static ILoggerFactory AddSpectreConsole(this ILoggerFactory factory, SpectreConsoleLoggerOptions? options = null) {
+            if (factory == null) {
+                throw new ArgumentNullException(nameof(factory));
+            }
+
+            factory.AddProvider(new SpectreConsoleLoggerProvider(options));
+            return factory;
+        }
+
+
+        /// <summary>
+        /// Adds a logger to the builder that writes to the console using Spectre.Console.
+        /// </summary>
+        /// <param name="builder">
+        ///   The <see cref="ILoggingBuilder"/>.
+        /// </param>
+        /// <returns>
+        ///   The <see cref="ILoggingBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="builder"/> is <see langword="null"/>.
+        /// </exception>
+        public static ILoggingBuilder AddSpectreConsole(this ILoggingBuilder builder) {
+            if (builder == null) {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, SpectreConsoleLoggerProvider>());
+            return builder;
+        }
+
+
+        /// <summary>
+        /// Adds a logger to the builder that writes to the console using Spectre.Console.
+        /// </summary>
+        /// <param name="builder">
+        ///   The <see cref="ILoggingBuilder"/>.
+        /// </param>
+        /// <param name="configure">
+        ///   A delegate that configures the <see cref="SpectreConsoleLoggerOptions"/> for the logger.
+        /// </param>
+        /// <returns>
+        ///   The <see cref="ILoggingBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="builder"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="configure"/> is <see langword="null"/>.
+        /// </exception>
+        public static ILoggingBuilder AddSpectreConsole(this ILoggingBuilder builder, Action<SpectreConsoleLoggerOptions> configure) {
+            if (builder == null) {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            if (configure == null) {
+                throw new ArgumentNullException(nameof(configure));
+            }
+
+            builder.AddSpectreConsole();
+            builder.Services.Configure(configure);
+
+            return builder;
+        }
+
+    }
+}
---
+++ src/Jaahas.Spectre.Extensions.Logging/SpectreConsoleLogger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+using Spectre.Console;
+
+namespace Jaahas.Spectre.Extensions.Logging {
+
+    /// <summary>
+    /// <see cref="ILogger"/> that writes to the console using <see cref="AnsiConsole"/>.
+    /// </summary>
+    internal sealed class SpectreConsoleLogger : ILogger {
+
+        /// <summary>
+        /// The formatter to use when writing log messages.
+        /// </summary>
+        internal SpectreConsoleFormatter Formatter { get; set; } = default!;
+
+
+        /// <summary>
+        /// Creates a new <see cref="SpectreConsoleLogger"/> with the specified formatter.
+        /// </summary>
+        /// <param name="formatter">
+        ///   The formatter to use when writing log messages.
+        /// </param>
+        public SpectreConsoleLogger(SpectreConsoleFormatter formatter) {
+            Formatter = formatter;
+        }
+
+
+        /// <inheritdoc/>
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) {
+            if (!IsEnabled(logLevel)) {
+                return;
+            }
+
+            // Do not pass the exception here; we'll write it below using AnsiConsole.WriteException
+            // if it is non-null.
+            var message = formatter(state, null);
+            Formatter.WriteMessage(logLevel, message, exception);
+        }
+
+
+        /// <inheritdoc/>
+        public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None && (!Formatter.MinimumLogLevel.HasValue || logLevel >= Formatter.MinimumLogLevel.Value);
+
+
+        /// <inheritdoc/>
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
+
+    }
+}
